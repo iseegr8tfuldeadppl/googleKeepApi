a7c6ce2d7ecfd48fc90df9c8c12d76a3f63602fd
K R E A M <45693917+iseegr8tfuldeadppl@users.noreply.github.com>
2023-08-13 12:02:00 +0100

    used #2, will need to code smt to copy only notes with cancel icon while making sure they're not duped

---
+++ openGoogleKeepAPI.bat
@@ -0,0 +1,8 @@
+title Google Keep API
+
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+cd C:\Users\kream\Desktop\googleKeepApi
+jupyter lab
+cmd \k
